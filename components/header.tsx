a22b255f942a8046afbed9ccd684a6f6de9f7a51
Leo Ji <leo@leoji.codes>
2020-08-29 23:58:23 -0400

    my name is now a link

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link'
+
 export function Header() {
   return (
     <header className='mb-5'>
-      <h1 className='display-1'>Leo Ji</h1>
+      <h1 className='display-1'>
+        <Link href='/'>Leo Ji</Link>
+      </h1>
       <h2>just another person on the Internet</h2>
     </header>
   )
