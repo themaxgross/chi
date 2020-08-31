84a080516958d2028dd54647045e179f5ef05c51
Leo Ji <leo@leoji.codes>
2020-08-31 00:41:48 -0400

    upgrade next, include headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 module.exports = {
   trailingSlash: true,
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self' font-src 'self' https://use.typekit.net",
+          },
+        ],
+      },
+    ];
+  },
 };
